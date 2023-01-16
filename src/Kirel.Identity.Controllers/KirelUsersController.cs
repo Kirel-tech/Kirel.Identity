32bdc29a16acc57dfdef57ee56726f0adf3c4c24
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2023-01-16 08:12:03 +0300

    controllers: users: add correct TRole type parameter
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

--- src/Kirel.Identity.Controllers/KirelUsersController.cs
+++ src/Kirel.Identity.Controllers/KirelUsersController.cs
@@ -9,19 +9,21 @@ namespace Kirel.Identity.Controllers;
 /// <summary>
 /// Controller for user management
 /// </summary>
-/// <typeparam name="TUserService">User management service</typeparam>
+/// <typeparam name="TUserService">User management service type</typeparam>
 /// <typeparam name="TKey">Id type</typeparam>
-/// <typeparam name="TUser">User type. Must be an implementation of the IApplicationUser interface</typeparam>
-/// <typeparam name="TUserDto">User dto</typeparam>
-/// <typeparam name="TUserCreateDto">User create dto. Must be a descendant of the UserCreateDto class</typeparam>
-/// <typeparam name="TUserUpdateDto">User update dto. Must be a descendant of the UserUpdateDto class</typeparam>
-/// <typeparam name="TClaimDto">Claim dto. Must be a descendant of the KirelClaimDto class</typeparam>
-/// <typeparam name="TClaimCreateDto">Claim create dto. Must be a descendant of the KirelClaimCreateDto class</typeparam>
-/// <typeparam name="TClaimUpdateDto">Claim update dto. Must be a descendant of the KirelClaimUpdateDto class</typeparam>
-public class KirelUsersController<TUserService,TKey,TUser,TUserDto,TUserCreateDto,TUserUpdateDto,TClaimDto,TClaimCreateDto,TClaimUpdateDto> : Controller
-    where TUserService : KirelUserService<TKey,TUser,TUserDto,TUserCreateDto,TUserUpdateDto,TClaimDto,TClaimCreateDto,TClaimUpdateDto>
+/// <typeparam name="TUser">User type. Must be a descendant of the KirelIdentityUser class</typeparam>
+/// <typeparam name="TRole">Role type. Must be a descendant of the KirelIdentityRole class</typeparam>
+/// <typeparam name="TUserDto">User dto type</typeparam>
+/// <typeparam name="TUserCreateDto">User create dto type. Must be a descendant of the UserCreateDto class</typeparam>
+/// <typeparam name="TUserUpdateDto">User update dto type. Must be a descendant of the UserUpdateDto class</typeparam>
+/// <typeparam name="TClaimDto">Claim dto type. Must be a descendant of the KirelClaimDto class</typeparam>
+/// <typeparam name="TClaimCreateDto">Claim create dto type. Must be a descendant of the KirelClaimCreateDto class</typeparam>
+/// <typeparam name="TClaimUpdateDto">Claim update dto type. Must be a descendant of the KirelClaimUpdateDto class</typeparam>
+public class KirelUsersController<TUserService,TKey,TUser, TRole,TUserDto,TUserCreateDto,TUserUpdateDto,TClaimDto,TClaimCreateDto,TClaimUpdateDto> : Controller
+    where TUserService : KirelUserService<TKey,TUser, TRole,TUserDto,TUserCreateDto,TUserUpdateDto,TClaimDto,TClaimCreateDto,TClaimUpdateDto>
     where TKey : IComparable, IComparable<TKey>, IEquatable<TKey> 
     where TUser : KirelIdentityUser<TKey>
+    where TRole : KirelIdentityRole<TKey>
     where TUserDto : KirelUserDto<TKey, TKey, TClaimDto>
     where TUserCreateDto : KirelUserCreateDto<TKey, TClaimCreateDto>
     where TUserUpdateDto : KirelUserUpdateDto<TKey, TClaimUpdateDto>
