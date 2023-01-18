76e3b22bed4cf78cd621e5c9d1db6a72ebd26a40
Konstantin Gamayunov <ksgamayunov@gmail.com>
2023-01-18 18:19:11 +0300

    middlewares: added new middlewares project and error handling middleware in it which set http response code depending on the exception type
    Signed-off-by: Konstantin Gamayunov <ksgamayunov@gmail.com>

---
+++ src/Kirel.Identity.Middlewares/KirelErrorHandlerMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using Kirel.Identity.Exceptions;
+using Microsoft.AspNetCore.Http;
+
+namespace Kirel.Identity.Middlewares;
+
+/// <summary>
+/// Middleware for global error handling
+/// </summary>
+public class KirelErrorHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    /// <summary>
+    /// Constructor of ErrorHandlerMiddleware
+    /// </summary>
+    /// <param name="next">RequestDelegate</param>
+    public KirelErrorHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    /// <summary>
+    /// Middleware call point
+    /// </summary>
+    /// <param name="context">HttpContext</param>
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (KirelIdentityStoreException)
+        {
+            var response = context.Response;
+            response.StatusCode = StatusCodes.Status500InternalServerError;
+        }
+        catch (KirelValidationException)
+        {
+            var response = context.Response;
+            response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+        catch (KirelAlreadyExistException)
+        {
+            var response = context.Response;
+            response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+        catch (KirelAuthenticationException)
+        {
+            var response = context.Response;
+            response.StatusCode = StatusCodes.Status401Unauthorized;
+        }
+        catch (KirelUnauthorizedException)
+        {
+            var response = context.Response;
+            response.StatusCode = StatusCodes.Status401Unauthorized;
+        }
+        catch (KirelNotFoundException)
+        {
+            var response = context.Response;
+            response.StatusCode = StatusCodes.Status404NotFound;
+        }
+    }
+}
