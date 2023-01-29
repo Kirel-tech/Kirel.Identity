7d65b4f8550bc2d31ed558721e8294373c04353e
Insei <goodmobiledevices@gmail.com>
2023-01-29 16:32:19 +0300

    example: blazor: jwt: add named http client for authorized identity requests
    Signed-off-by: Insei <goodmobiledevices@gmail.com>

--- src/Example.Blazor.Jwt/Program.cs
+++ src/Example.Blazor.Jwt/Program.cs
@@ -4,11 +4,16 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Example.Blazor.Jwt;
 using Kirel.Identity.Client.Blazor.Services;
 using Kirel.Identity.Client.Interfaces;
+using Kirel.Identity.Client.Jwt.Handlers;
 using Kirel.Identity.Client.Jwt.Options;
 using Kirel.Identity.Client.Jwt.Providers;
 using Kirel.Identity.Client.Jwt.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor.Services;
+
+// From Example.API project /Properties/launchSettings.json profiles -> Example
+const string identityUriStr = "https://localhost:7264/";
+var identityUri = new Uri(identityUriStr);
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -20,14 +25,19 @@ builder.Services.AddMudServices();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<IClientTokenService, KirelBlazoredClientTokenService>();
 
-// Add http client factory instance, it's used by KirelClientJwtAuthenticationService
+// Add http client factory instance with default http client for fetch data example
 builder.Services.AddHttpClient(string.Empty, hc => hc.BaseAddress = new Uri("https://localhost:7005"));
+
+// Add Identity http client with JWT Authorization handler to factory
+builder.Services.AddScoped<KirelJwtHttpClientAuthorizationHandler>();
+builder.Services.AddHttpClient("IdentityAuthorized", hc => hc.BaseAddress = identityUri)
+    .AddHttpMessageHandler<KirelJwtHttpClientAuthorizationHandler>();
 
 // Add JWT authentication service with configuration for working with Example project API endpoint
 builder.Services.AddScoped<IClientAuthenticationService, KirelClientJwtAuthenticationService>();
 builder.Services.Configure<KirelClientJwtAuthenticationOptions>(options =>
 {
-    options.BaseUrl = "https://localhost:7264/"; // From Example project /Properties/launchSettings.json profiles -> Example
+    options.BaseUrl = identityUriStr;
     options.RelativeUrl = "authentication/jwt"; // From Example project /Controllers/ExJwtAuthenticationController Route Attribute
 });
 
