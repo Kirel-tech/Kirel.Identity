1d6d55c87c50df7ba909e549cdb2d59ec39db884
Insei <goodmobiledevices@gmail.com>
2023-01-29 16:27:44 +0300

    client: jwt: add http client jwt authorization handler
    Signed-off-by: Insei <goodmobiledevices@gmail.com>

---
+++ src/Kirel.Identity.Client.Jwt/Handlers/KirelJwtHttpClientAuthorizationHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http.Headers;
+using Kirel.Identity.Client.Interfaces;
+using Kirel.Identity.Client.Jwt.Helpers;
+
+namespace Kirel.Identity.Client.Jwt.Handlers;
+
+public class KirelJwtHttpClientAuthorizationHandler : DelegatingHandler
+{
+    private readonly IClientTokenService _tokenService;
+    private readonly IClientAuthenticationService _authenticationService;
+    public KirelJwtHttpClientAuthorizationHandler(IClientTokenService tokenService, IClientAuthenticationService authenticationService)
+    {
+        _tokenService = tokenService;
+        _authenticationService = authenticationService;
+    }
+
+    /// <inheritdoc />
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var jwtAccessToken = await _tokenService.GetAccessTokenAsync();
+        if (KirelJwtTokenHelper.TokenIsExpired(jwtAccessToken))
+        {
+            await _authenticationService.ExtendSessionAsync();
+            jwtAccessToken = await _tokenService.GetAccessTokenAsync();
+        }
+        
+        request.Headers.Authorization =
+            new AuthenticationHeaderValue("Bearer", jwtAccessToken);
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
