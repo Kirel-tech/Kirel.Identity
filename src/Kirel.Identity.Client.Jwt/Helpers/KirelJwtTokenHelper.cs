a6f16e60a81829089e3c6326aef646fd6b7a2ea5
Insei <goodmobiledevices@gmail.com>
2023-01-29 16:23:30 +0300

    client: jwt: now GetExpirationTime method in KirelJWTTokenHelper return DateTime in UTC
    Signed-off-by: Insei <goodmobiledevices@gmail.com>

--- src/Kirel.Identity.Client.Jwt/Helpers/KirelJwtTokenHelper.cs
+++ src/Kirel.Identity.Client.Jwt/Helpers/KirelJwtTokenHelper.cs
@@ -21,8 +21,8 @@ public static class KirelJwtTokenHelper
     /// Allows you to get the expiration time of the JWT token.
     /// </summary>
     /// <param name="jwtToken">JWT token string.</param>
-    /// <returns>DateTimeOffset with expiration time</returns>
-    public static DateTimeOffset GetExpirationTime(string jwtToken)
+    /// <returns><see cref="DateTime"/> with expiration time in UTC</returns>
+    public static DateTime GetExpirationTime(string jwtToken)
     {
         var token = new JwtSecurityTokenHandler().ReadJwtToken(jwtToken);
         return token.ValidTo.ToUniversalTime();
