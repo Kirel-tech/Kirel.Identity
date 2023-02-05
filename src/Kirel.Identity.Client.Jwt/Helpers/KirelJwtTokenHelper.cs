453f08a370302f734ae390f6f88c1b449b9a306c
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2023-02-05 04:37:03 +0300

    client: jwt: removed the 10 second time margin when checking the token expiration time
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

--- src/Kirel.Identity.Client.Jwt/Helpers/KirelJwtTokenHelper.cs
+++ src/Kirel.Identity.Client.Jwt/Helpers/KirelJwtTokenHelper.cs
@@ -15,7 +15,7 @@ public static class KirelJwtTokenHelper
     public static bool TokenIsExpired(string jwtToken)
     {
         var token = new JwtSecurityTokenHandler().ReadJwtToken(jwtToken);
-        return token.ValidTo.ToUniversalTime() < DateTime.UtcNow.AddSeconds(10);
+        return token.ValidTo.ToUniversalTime() < DateTime.UtcNow;
     }
     /// <summary>
     /// Allows you to get the expiration time of the JWT token.
