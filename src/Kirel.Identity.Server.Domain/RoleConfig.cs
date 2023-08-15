7dd33df85568f7fcd754863bc568486c7bae00e2
Vova Zelenyi <vova2010201040@gmail.com>
2023-08-15 17:22:05 +0200

    Adding Configuration for Roles and Users
    Added configuration for creating roles and users

---
+++ src/Kirel.Identity.Server.Domain/RoleConfig.cs
@@ -0,0 +1,18 @@
+namespace Kirel.Identity.Server.Domain
+{
+    /// <summary>
+    /// Configuration class for roles
+    /// </summary>
+    public class RoleConfig
+    {
+        /// <summary>
+        /// Gets or sets the role name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the normalized role name.
+        /// </summary>
+        public string NormalizedName { get; set; }
+    }
+}
