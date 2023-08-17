b6a5518d564c339ddeba08ec6dd7250eaeece3e6
Vova Zelenyi <vova2010201040@gmail.com>
2023-08-17 14:03:51 +0200

    add instruction and add filters to program.cs

--- src/Kirel.Identity.Server.API/Program.cs
+++ src/Kirel.Identity.Server.API/Program.cs
@@ -1,5 +1,7 @@
 using Kirel.Identity.Middlewares;
+using Kirel.Identity.Server.API.Controllers;
 using Kirel.Identity.Server.API.Extensions;
+using Kirel.Identity.Server.API.Filters;
 using Kirel.Identity.Server.Core.Extensions;
 using Kirel.Identity.Server.Domain;
 using Kirel.Identity.Server.Infrastructure.Contexts;
@@ -13,6 +15,13 @@ var builder = WebApplication.CreateBuilder(args);
 var jwtConfig = builder.Configuration.GetSection("jwt").Get<JwtAuthenticationConfig>();
 var dbConfig = builder.Configuration.GetSection("dbConfig").Get<DbConfig>();
 var maintenanceConfig = builder.Configuration.GetSection("maintenance").Get<MaintenanceConfig>();
+var registrationDisabled = builder.Configuration.GetValue<bool>("RegistrationDisabled");
+
+//To disable controller add him here like disabledControllers.Add(typeof(RegistrationController));
+var disabledControllers = new DisabledControllerTypes();
+if (registrationDisabled) disabledControllers.Add(typeof(RegistrationController));
+
+
 
 // Add Identity framework db context based on Kirel Identity templates
 // with ability to change db driver (mssql, mysql, postgresql)
@@ -37,15 +46,17 @@ builder.Services.AddMappers();
 builder.Services.AddValidators();
 // Add Identity services and jwt token issuing service, that works with DTOs and with fluent validation framework
 builder.Services.AddServices(jwtConfig);
-
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(new EnabledControllerActionFilter(disabledControllers));
+});
 
 
 // Add ASP.NET authentication configuration
 builder.Services.AddAuthenticationConfiguration(jwtConfig);
 
 // Add custom swagger configuration
-builder.Services.AddSwagger();
+builder.Services.AddSwagger(disabledControllers);
 
 builder.Services.AddCors(options =>
 {
