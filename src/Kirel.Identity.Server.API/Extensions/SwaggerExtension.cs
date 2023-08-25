5099e532887ac057ff3d5e5828f7aa57b70b86b9
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2023-08-25 10:56:25 +0300

    src: runtime: api: add api key authentication support
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

--- src/Kirel.Identity.Server.API/Extensions/SwaggerExtension.cs
+++ src/Kirel.Identity.Server.API/Extensions/SwaggerExtension.cs
@@ -39,6 +39,15 @@ public static class SwaggerExtension
                 Description =
                     "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\""
             });
+            c.AddSecurityDefinition("APIKey", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "APIKey",
+                In = ParameterLocation.Header,
+                Description =
+                    "API key authorization header using the APIKey scheme. \r\n\r\n Enter 'APIKey' [space] and then your API key in the text input below.\r\n\r\nExample: \"APIKey 12345abcdef\""
+            });
             c.AddSecurityRequirement(new OpenApiSecurityRequirement
             {
                 {
@@ -48,6 +57,17 @@ public static class SwaggerExtension
                         {
                             Type = ReferenceType.SecurityScheme,
                             Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                },
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "APIKey"
                         }
                     },
                     new string[] { }
--- src/Kirel.Identity.Server.API/Program.cs
+++ src/Kirel.Identity.Server.API/Program.cs
@@ -2,6 +2,7 @@ using Kirel.Identity.Middlewares;
 using Kirel.Identity.Server.API.Controllers;
 using Kirel.Identity.Server.API.Extensions;
 using Kirel.Identity.Server.API.Filters;
+using Kirel.Identity.Server.API.Handlers;
 using Kirel.Identity.Server.Core.Extensions;
 using Kirel.Identity.Server.Domain;
 using Kirel.Identity.Server.Infrastructure.Contexts;
@@ -16,25 +17,17 @@ var jwtConfig = builder.Configuration.GetSection("jwt").Get<JwtAuthenticationCon
 var dbConfig = builder.Configuration.GetSection("dbConfig").Get<DbConfig>();
 var maintenanceConfig = builder.Configuration.GetSection("maintenance").Get<MaintenanceConfig>();
 var registrationDisabled = builder.Configuration.GetValue<bool>("RegistrationDisabled");
+var apiKeys = builder.Configuration.GetSection("APIKeys").Get<ApiKeysList>();
 
 //To disable controller add him here like disabledControllers.Add(typeof(RegistrationController));
 var disabledControllers = new DisabledControllerTypes();
 if (registrationDisabled) disabledControllers.Add(typeof(RegistrationController));
 
-builder.Services.Configure<DataProtectionTokenProviderOptions>(options =>
-{
-    options.TokenLifespan = TimeSpan.FromHours(90); // Настройте желаемое время жизни токена
-});
-
-
 // Add Identity framework db context based on Kirel Identity templates
 // with ability to change db driver (mssql, mysql, postgresql)
 builder.Services.AddDbContextByConfig<IdentityServerDbContext, MssqlIdentityServerDbContext,
     PostgresqlIdentityServerDbContext, MysqlIdentityServerDbContext, SqliteIdentityServerDbContext>(dbConfig);
-builder.Services.AddIdentity<User, Role>()
-    .AddEntityFrameworkStores<IdentityServerDbContext>()
-    .AddDefaultTokenProviders();
-
+builder.Services.AddIdentity<User, Role>().AddEntityFrameworkStores<IdentityServerDbContext>();
 builder.Services.Configure<IdentityOptions>(options =>
 {
     // Password settings.
@@ -42,11 +35,9 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequireLowercase = true;
     options.Password.RequireNonAlphanumeric = true;
     options.Password.RequireUppercase = true;
-    /*options.SignIn.RequireConfirmedEmail = true;*/
+
     // User settings.
-    options.User.RequireUniqueEmail = true;
-    //options.Tokens.EmailConfirmationTokenProvider = "Email";
-    //options.Tokens.
+    options.User.RequireUniqueEmail = false;
 });
 
 // Add identity mappers
@@ -62,7 +53,7 @@ builder.Services.AddControllers(options =>
 
 
 // Add ASP.NET authentication configuration
-builder.Services.AddAuthenticationConfiguration(jwtConfig);
+builder.Services.AddAuthenticationConfiguration(jwtConfig, apiKeys);
 
 // Add custom swagger configuration
 builder.Services.AddSwagger(disabledControllers);
