fca2f7d244a6ec78eb82e2d68a8ba8ca25baedb9
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2023-08-27 15:10:54 +0300

    src: runtime: server: add migrations and maintenance extensions
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

---
+++ src/Kirel.Identity.Server.API/Extensions/MaintenanceExtension.cs
@@ -0,0 +1,66 @@
+﻿using Kirel.Identity.Core.Models;
+using Kirel.Identity.Server.Domain;
+using Microsoft.AspNetCore.Identity;
+
+namespace Kirel.Identity.Server.API.Extensions;
+
+/// <summary>
+/// Maintenance application builder extension
+/// </summary>
+public static class MaintenanceExtension
+{
+    /// <summary>
+    /// Do user data maintenance
+    /// </summary>
+    /// <param name="app"> Application builder </param>
+    /// <param name="config"> Maintenance config </param>
+    /// <typeparam name="TKey"> User entity key type. </typeparam>
+    /// <typeparam name="TUser"> User entity type. </typeparam>
+    /// <typeparam name="TRole"> Role entity type. </typeparam>
+    /// <typeparam name="TUserRole"> Role user entity type. </typeparam>
+    public static async Task MaintenanceAsync<TKey, TUser, TRole, TUserRole>(
+        this IApplicationBuilder app, MaintenanceConfig config)
+        where TRole : KirelIdentityRole<TKey, TRole, TUser, TUserRole>, new ()
+        where TUser : KirelIdentityUser<TKey, TUser, TRole, TUserRole>, new ()
+        where TKey : IComparable, IComparable<TKey>, IEquatable<TKey>
+        where TUserRole : KirelIdentityUserRole<TKey, TUserRole, TUser, TRole>
+    {
+        var scopedServiceProvider = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope()
+            .ServiceProvider;
+        var roleManager = scopedServiceProvider.GetRequiredService<RoleManager<TRole>>();
+        var userManager = scopedServiceProvider.GetRequiredService<UserManager<TUser>>();
+        var adminRoleExist = true;
+        var adminRole = await roleManager.FindByNameAsync("Admin");
+        if (adminRole == null)
+        {
+            adminRole = new TRole { Name = "Admin", NormalizedName = "ADMIN" };
+            var result = await roleManager.CreateAsync(adminRole);
+            if (!result.Succeeded)
+                adminRoleExist = false;
+        }
+
+        var adminUserExist = true;
+        var adminUser = await userManager.FindByNameAsync("Admin");
+        if (adminUser == null)
+        {
+            adminUser = new TUser
+            {
+                UserName = "Admin", Email = "admin@citmed.ru", Name = "Admin", LastName = "Default"
+            };
+            var result = await userManager.CreateAsync(adminUser, "Admin@123");
+            if (!result.Succeeded)
+                adminUserExist = false;
+        }
+
+        if (adminUserExist && config.Admin.Reset)
+        {
+            var newPassword = config.Admin.Password != "" ? config.Admin.Password : "Admin@123";
+            await userManager.RemovePasswordAsync(adminUser);
+            await userManager.AddPasswordAsync(adminUser, newPassword);
+        }
+
+        if (!adminUserExist || !adminRoleExist) return;
+        if (!await userManager.IsInRoleAsync(adminUser, "Admin"))
+            await userManager.AddToRoleAsync(adminUser, "Admin");
+    }
+}
---
+++ src/Kirel.Identity.Server.Infrastructure/Extensions/MigrationsExtension.cs
@@ -0,0 +1,24 @@
+﻿using Kirel.Identity.Server.Infrastructure.Contexts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kirel.Identity.Server.Infrastructure.Extensions;
+
+/// <summary>
+/// Identity db migrations Application Builder extension
+/// </summary>
+public static class MigrationsExtension
+{
+    /// <summary>
+    /// Apply identity db context migration
+    /// </summary>
+    /// <param name="app"> Application builder </param>
+    public static async Task MigrateIdentityDbAsync(this IApplicationBuilder app)
+    {
+        var scopedServiceProvider = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope()
+            .ServiceProvider;
+        var dbContext = scopedServiceProvider.GetRequiredService<IdentityServerDbContext>();
+        await dbContext.Database.MigrateAsync();
+    }
+}
