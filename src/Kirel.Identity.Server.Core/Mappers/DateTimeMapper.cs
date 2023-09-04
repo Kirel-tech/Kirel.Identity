7b1e43580ba3efc0e6233eb9eeb8a855f224e2e7
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2023-09-04 20:58:25 +0300

    src: runtime: core: add date time mappers
    * I can't figure out why but without this we don't get the correct encoding of dates in json in UTC format. Without this fix, we get dates without the "Z" at the end, but all the dates that are currently in the system are accurately stored in UTC.
    
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

---
+++ src/Kirel.Identity.Server.Core/Mappers/DateTimeMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+namespace Kirel.Identity.Server.Core.Mappers;
+
+/// <summary>
+/// Mapping profile for date time for converting to UTC
+/// </summary>
+public class DateTimeMapper : Profile
+{
+    /// <summary>
+    /// DateTimeMapper constructor
+    /// </summary>
+    public DateTimeMapper()
+    {
+        CreateMap<DateTime, DateTime>()
+            .ConvertUsing((s, d) => s.ToUniversalTime());
+        CreateMap<DateTime?, DateTime?>()
+            .ConvertUsing((s, d) => s?.ToUniversalTime());
+    }
+}
